30f63350218ecd51e385e90ae05f8e3f17d4b0b9
Matvey Kham <mvham@mail.ru>
2022-08-04 17:07:05 +0700

    feat: add login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Login from '../Login/Login';
 import Main from '../Main/Main';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Main />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/sign-up' element={<Register />} />
+        <Route path='/sign-in' element={<Login />} />
       </Routes>
     </div>
   );
