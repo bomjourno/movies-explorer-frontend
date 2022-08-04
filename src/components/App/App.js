3c98a6a3192c04d875606bd1d9a486d29c134535
Matvey Kham <mvham@mail.ru>
2022-08-04 18:07:08 +0700

    feat: add not found page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from '../Login/Login';
 import Main from '../Main/Main';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
         <Route path='/profile' element={<Profile />} />
         <Route path='/sign-up' element={<Register />} />
         <Route path='/sign-in' element={<Login />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </div>
   );
